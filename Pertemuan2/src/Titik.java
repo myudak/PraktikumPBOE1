e155381bbf7931f0ae797aef59e646b402f4dda4
myudak <yudaplzhacker@gmail.com>
2026-02-27 09:27:32 +0700

    menyelesaikan semua implementasi fungsi untuk titik dan sebagian untuk garis

--- Pertemuan2/src/Titik.java
+++ Pertemuan2/src/Titik.java
@@ -6,8 +6,8 @@
 
 public class Titik {
     /**************** ATRIBUT ******************/
-    double absis;
-    double ordinat;
+    private double absis;
+    private double ordinat;
     static int counterTitik = 0;
 
     /**************** METHOD ******************/
@@ -25,44 +25,94 @@ public class Titik {
     }
 
     // Mengembalikan nilai absis
-    double getAbsis() {
+    public double getAbsis() {
         return absis;
     }
 
     // Mengembalikan nilai ordinat
-    double getOrdinat() {
+    public double getOrdinat() {
         return ordinat;
     }
 
     // Mengeset absis titik dengan nilai baru x
-    void setAbsis(double x) {
+    public void setAbsis(double x) {
         absis = x;
     }
 
     // Mengeset ordinat titik dengan nilai baru y
-    void setOrdinat(double y) {
+    public void setOrdinat(double y) {
         ordinat = y;
     }
 
     // Menggeser nilai absis dan ordinat titik masing-masing sejauh x dan y
-    void geser(double x, double y) {
+    public void geser(double x, double y) {
         absis = absis + x;
         ordinat = ordinat + y;
     }
 
     // Mencetak koordinat titik
-    void printTitik() {
+    public void printTitik() {
         System.out.println("Titik (" + absis + "," + ordinat + ")");
     }
 
     // mengembalikan nilai counterTitik
-    static int getCounterTitik() {
+    public static int getCounterTitik() {
         return counterTitik;
     }
 
-    void printCounterTitik() {
+    public void printCounterTitik() {
         // System.out.println(this.counterTitik);
         System.out.println(counterTitik);
     }
 
-} // end class Titik
+    // mengembalikan nilai kuadran titik
+    public int getKuadran() {
+        if (getAbsis() > 0 && getOrdinat() > 0) {
+            return 1;
+        } else if (getAbsis() < 0 && getOrdinat() > 0) {
+            return 2;
+        } else if (getAbsis() < 0 && getOrdinat() > 0) {
+            return 3;
+        } else if (getAbsis() > 0 && getOrdinat() < 0) {
+            return 4;
+        } else {
+            return 0;
+        }
+    }
+
+    // mengembalikan jarak titik dari titik pusat
+    public double getJarakPusat() {
+        return Math.sqrt(Math.pow(getAbsis(), 2) + Math.pow(getOrdinat(), 2));
+    }
+
+    // mengembalikan jarak titik dari titik lainnya
+    public double getJarak(Titik T) {
+        return Math.sqrt(Math.pow(this.getAbsis() - T.getAbsis(), 2) + Math.pow(this.getOrdinat() - T.getOrdinat(), 2));
+    }
+
+    // mengubah titik menjadi titik refleksi terhadap sumbu X
+    public void refleksiX() {
+        setOrdinat(getOrdinat() * -1);
+    }
+
+    // mengubah titik menjadi titik refleksi terhadap sumbu Y
+    public void refleksiY() {
+        setAbsis(getAbsis() * -1);
+    }
+
+    // mengembalikan titik hasil refleksi terhadap sumbu X
+    public Titik getRekfleksiX() {
+        Titik THasil = new Titik(getAbsis(), getOrdinat());
+        refleksiX();
+        return THasil;
+    }
+
+    // mengembalikan titik hasil refleksi terhadap sumbu Y
+    public Titik getRefleksiY() {
+        Titik THasil = new Titik(getAbsis(), getOrdinat());
+        refleksiY();
+        return THasil;
+    }
+
+}
+// end class Titik
