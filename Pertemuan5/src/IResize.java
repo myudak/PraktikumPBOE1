4cbef7e70927d84d7d4a4ddb96f7ba937d7aa3e7
myudak <yudaplzhacker@gmail.com>
2026-03-20 14:11:44 +0700

    Menyelesaikan implementasi studi kasusu praktikum 5

---
+++ Pertemuan5/src/IResize.java
@@ -0,0 +1,14 @@
+/**
+ * File : IResize.java
+ * Deskripsi : Class interface untuk melakukan resize
+ * Nama : Muchammad Yuda Tri Ananda
+ * Tanggal : 18 Maret 2026
+ */
+
+public interface IResize {
+    public void zoomIn();
+
+    public void zoomOut();
+
+    public void zoom(int percent);
+}
