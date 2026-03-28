060cc2b54e479dce9d4e2d8ba51bbdcb1cbfa279
myudak <yudaplzhacker@gmail.com>
2026-03-28 14:14:23 +0700

    Menyelesaikan semua implementasi di pertemuan 6

---
+++ Pertemuan6/src/ExceptionOnArray.java
@@ -0,0 +1,13 @@
+public class ExceptionOnArray {
+    public static void main(String[] args) {
+        Integer[] arrayIntegers = new Integer[4];
+        try {
+            arrayIntegers[2] = 11;
+            arrayIntegers[4] = 10;
+        } catch (ArrayIndexOutOfBoundsException exception) {
+            exception.printStackTrace();
+        } finally {
+            System.out.println("clean up code...");
+        }
+    }
+}
