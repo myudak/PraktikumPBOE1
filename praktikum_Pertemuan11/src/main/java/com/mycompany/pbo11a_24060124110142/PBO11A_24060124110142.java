49a8ae850c9d9d562c483ea7839048753572cf6f
myudak <yudaplzhacker@gmail.com>
2026-06-04 18:53:59 +0700

    pertemuan 11

---
+++ praktikum_Pertemuan11/src/main/java/com/mycompany/pbo11a_24060124110142/PBO11A_24060124110142.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pbo11a_24060124110142;
+
+/**
+ *
+ * @author Muchammad Yuda Tri Ananda
+ */
+public class PBO11A_24060124110142 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
